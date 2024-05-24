e5e63a4c2e08f288b9cc60d637ebcc0f451f9f6d
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-05-24 10:25:53 +0200

    Created WelfareCard component, to be dynamically generated in CorporateWelfare component, with data passed via props.

---
+++ src/components/WelfareCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="welfare-card">
+        <img :src="imgUrl" :alt="label">
+        <p class="bold">{{ label }}</p>
+        <p>{{ text }}</p>
+        <button>Scopri di più</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'WelfareCard',
+
+        props: {
+            label: String,
+            text: String,
+            imgUrl: String,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
+    div.welfare-card {
+        
+    }
+</style>
