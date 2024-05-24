770f42b158ed2c2ea138efd088043837bf7ae4ff
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-05-24 17:40:51 +0200

    CollaboratorsWellness component styling completed for both desktop & mobile.

--- src/components/CollaboratorsWellness.vue
+++ src/components/CollaboratorsWellness.vue
@@ -21,7 +21,34 @@
     div#collaborators-wellness {
         background-image: url("/src/assets/go-plan-pattern.png");
         background-color: map-get(map-get($colors, Primary), 700);
-        color: map-get(map-get($colors, Black), 50);
-        text-align: center;
+        padding: 8rem 1rem;
+
+        @media screen and (max-width: 767px) {
+            padding: 8rem 2rem;
+        }
+
+        div.container {
+            @include flex(column, center, center, wrap);
+            color: map-get(map-get($colors, Black), 50);
+            margin: auto;
+            text-align: center;
+
+            h4 {
+                font-size: 38px;
+                margin-bottom: 1.5rem;
+            }
+
+            p:first-of-type {
+                margin-bottom: 1rem;
+            }
+
+            p:last-of-type {
+                width: 46%;
+
+                @media screen and (max-width: 767px) {
+                    width: 100%;
+                }
+            }
+        }
     }
 </style>
