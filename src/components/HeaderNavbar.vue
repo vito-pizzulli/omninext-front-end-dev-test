29b71dcba69060729b12731b47de8c206eab85a9
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-05-24 14:34:07 +0200

    HeaderNavbar component styling completed for both desktop & mobile.

--- src/components/HeaderNavbar.vue
+++ src/components/HeaderNavbar.vue
@@ -2,17 +2,17 @@
     <div id="header-navbar">
         <nav class="desktop">
             <ul>
-                <img src="/src/assets/logo.png" alt="MySarma Logo">
-                <li v-for="(headerElement, index) in headerElements" :key="index"><a :href="headerElement.link">{{ headerElement.label }}</a></li>
-                <li><a href="/login"><img src="/src/assets/icon-exit.png" alt="Access icon">Accedi alla piattaforma</a></li>
+                <li><img class="logo" src="/src/assets/logo-header.svg" alt="MySarma Logo"></li>
+                <li v-for="(headerElement, index) in headerElements" :key="index"><a class="bold" :href="headerElement.link">{{ headerElement.label }}</a></li>
+                <li><button class="login"><img src="/src/assets/icon-exit.svg" alt="Access icon">Accedi alla piattaforma</button></li>
             </ul>
         </nav>
 
         <nav class="mobile">
             <ul>
-                <li><img src="/src/assets/logo.png" alt="MySarma Logo"></li>
-                <li><a href="/login"><img src="/src/assets/icon-exit.png" alt="Access icon">Accedi</a></li>
-                <li><img src="/src/assets/icon-hamburger.png" alt="Hamburger icon"></li>
+                <li><img class="logo" src="/src/assets/logo-header.svg" alt="MySarma Logo"></li>
+                <li><button class="login"><img src="/src/assets/icon-exit.svg" alt="Access icon">Accedi</button></li>
+                <li><button><img src="/src/assets/icon-hamburger.svg" alt="Hamburger icon"></button></li>
             </ul>
         </nav>
     </div>
@@ -42,13 +42,53 @@
 
         nav {
             ul {
-                @include flex(row, space-between, center, wrap);
+                @include flex(row, space-between, center, no-wrap);
                 padding: .8rem;
+
+                li {
+                    button.login {
+                        background-color: map-get(map-get($colors, Green), 200);
+
+                        img {
+                            margin-right: .5rem;
+                        }
+                    }
+
+                    &:first-of-type {
+                        width: 20%;
+                    }
+                }
+
+                a {
+                    color: map-get(map-get($colors, Primary), 500);
+                }
             }
 
-            &.mobile ul li:first-of-type {
-                width: 50%;
+            &.mobile {
+                img.logo {
+                    width: 150px;
+                    margin-right: 2rem;
+                }
+
+                button.login {
+                    background-color: map-get(map-get($colors, Black), 50);
+                    color: map-get(map-get($colors, Green), 600);
+                    font-size: 1.2rem;
+
+                    img {
+                        filter: brightness(200%);
+                        width: 25px;
+                    }
+                }
+
+                ul li:first-of-type {
+                    width: 50%;
+                }
             }
+        }
+
+        @media screen and (max-width: 767px) {
+            width: 100%;
         }
     }
 </style>
